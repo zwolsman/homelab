a9692ffab8fc4e8230d0b3c5a5c768d669cc4c2d
Marvin Zwolsman <mzwolsman@bol.com>
2025-11-07 18:43:30 +0100

    fix(nixos): remove nvidia setup for homelab-4

--- nixos/hosts/server/homelab-4/default.nix
+++ nixos/hosts/server/homelab-4/default.nix
@@ -6,38 +6,39 @@
   ];
 
   system.stateVersion = "25.05";
-
-  hardware.nvidia.package = config.boot.kernelPackages.nvidia_x11_legacy470;
-  hardware.graphics.enable = true;
-  hardware.nvidia.datacenter.enable = true;
-  services.xserver.videoDrivers = true;
-  hardware.nvidia-container-toolkit.enable = true;
+  # TODO: fix nvidia driver for homelab-4 (Tesla P4 GPU)
+  #  add support in k3s for GPU accelerated workloads
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidia_x11_legacy470;
+  # hardware.graphics.enable = true;
+  # hardware.nvidia.datacenter.enable = true;
+  # services.xserver.videoDrivers = true;
+  # hardware.nvidia-container-toolkit.enable = true;
   
-  hardware.nvidia = {
+  # hardware.nvidia = {
 
-    # Modesetting is required.
-    modesetting.enable = true;
+  #   # Modesetting is required.
+  #   modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = false;
+  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+  #   # Enable this if you have graphical corruption issues or application crashes after waking
+  #   # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+  #   # of just the bare essentials.
+  #   powerManagement.enable = false;
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+  #   # Fine-grained power management. Turns off GPU when not in use.
+  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+  #   powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    open = false;
+  #   # Use the NVidia open source kernel module (not to be confused with the
+  #   # independent third-party "nouveau" open source driver).
+  #   # Support is limited to the Turing and later architectures. Full list of
+  #   # supported GPUs is at:
+  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+  #   # Only available from driver 515.43.04+
+  #   open = false;
 
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-  };
+  #   # Enable the Nvidia settings menu,
+  #   # accessible via `nvidia-settings`.
+  #   nvidiaSettings = true;
+  # };
 }
